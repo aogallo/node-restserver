ce75c29a8e50cb59acfb99ddd4c536b555be1e22
Allan <rockmarshal.2308host@gmail.com>
2018-05-30 17:41:17 -0600

    Variable de cambio de mongo put,delete,get

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,7 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+mongodb: //localhost:27017/cafe
+
+    mongodb: //<dbuser>:<dbpassword>@ds139950.mlab.com:39950/cafe
