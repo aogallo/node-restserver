282bd72b39be916864ec9d6b4eca732b8cf73f84
Allan <rockmarshal.2308host@gmail.com>
2018-06-01 08:44:12 -0600

    Variable de entorno de mongo de mLab

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Brie12345678@ds139950.mlab.com:39950/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
