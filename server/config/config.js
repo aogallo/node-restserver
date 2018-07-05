5248f0140418795151ad05409c7b43b160348c2d
Allan Gallo <rockmarshal.2308host@gmail.com>
2018-07-04 18:17:47 -0600

    Git login y configuracion de varibles

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
+import { homedir } from "os";
+
 // ============================
 //  Puerto
 // ============================
@@ -9,6 +11,19 @@ process.env.PORT = process.env.PORT || 3000;
 // ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
+// ============================
+//  Vencimiento de token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ============================
+//  SEED de autenticacion
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
 
 // ============================
 //  Base de datos
