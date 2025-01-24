4988c076937ee07f7898f8952b967c59813393fe
Allan Gallo <allan.gallo.guerra@gmail.com>
2025-01-24 10:29:34 -0600

    add jwt types

--- server/middlewares/autentication.ts
+++ server/middlewares/autentication.ts
@@ -1,52 +1,60 @@
-const jwt = require('jsonwebtoken');
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 //====================================
 //  VERIFICACION DE TOKEN
 //====================================
 
-let verificaToken = (req, res, next) => {
-    let token = req.get('token');
-
-    jwt.verify(token, process.env.SEED, (err, decoded) => {
-
-        if (err) {
-            return res.status(401).json({
-                ok: false,
-                message: "Token no valido"
-            });
-        }
-
-        req.usuario = decoded.usuario;
-
-        next();
-
+const verificaToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.get("token");
+  const secretKey = process.env.SEED;
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      message: "Invalid token",
     });
+  }
 
+  if (!secretKey) {
+    return res.status(500).json({
+      ok: false,
+      message: "Internal Server Error",
+    });
+  }
 
+  const decoded = jwt.verify(token, secretKey);
+
+  if (!decoded) {
+    return res.status(401).json({
+      ok: false,
+      message: "Invalid token",
+    });
+  }
+
+  req.usuario = decoded.usuario;
+
+  next();
 };
 
 //====================================
 //  VERIFICACION DE ADMINROLE
 //====================================
 
-let verificaAdmin_Role = (req, res, next) => {
+const verificaAdmin_Role = (req, res, next) => {
+  const usuario = req.usuario;
+  console.log(usuario);
 
-    let usuario = req.usuario;
-    console.log(usuario);
+  if (usuario.role != "ADMIN_ROLE") {
+    return res.status(404).json({
+      ok: false,
+      message: "El usuario no es administrador",
+    });
+  }
 
-    if (usuario.role != 'ADMIN_ROLE') {
-        return res.status(404).json({
-            ok: false,
-            message: "El usuario no es administrador"
-        });
-    }
-
-    next();
-
+  next();
 };
-
-
 
 module.exports = {
-    verificaToken,
-    verificaAdmin_Role
+  verificaToken,
+  verificaAdmin_Role,
 };
+
